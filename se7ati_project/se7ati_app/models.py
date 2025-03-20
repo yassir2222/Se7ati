f5be234e19553ce8916aab6adabce027283c1f16
yassir2222 <yassir.lambrass@gmail.com>
2025-03-20 00:06:35 +0000

    login/signe up

--- se7ati_project/se7ati_app/models.py
+++ se7ati_project/se7ati_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser): 
+    USER_TYPE_CHOICES = (
+        ('patient', 'Patient'),
+        ('doctor', 'Doctor'),
+    )
+    user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES, default='patient')
+
+    def __str__(self):
+        return self.username
+
+class Patient(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    medical_history = models.TextField(blank=True, null=True)
+    def __str__(self):
+        return self.user.username
+
+class Doctor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    specialization = models.CharField(max_length=255)
+    license_number = models.CharField(max_length=50)
+    def __str__(self):
+        return "DR "+self.user.username
