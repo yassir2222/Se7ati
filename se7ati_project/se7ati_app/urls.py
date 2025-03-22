654423dbf2719b2858174f5459ec0d7b3a19856e
yassir2222 <yassir.lambrass@gmail.com>
2025-03-22 23:35:36 +0000

    Edit user profil

--- se7ati_project/se7ati_app/urls.py
+++ se7ati_project/se7ati_app/urls.py
@@ -11,8 +11,10 @@ urlpatterns = [
     path('pharmacy/', views.chercher_pharmacies_view, name='chercher_pharmacies_view'),
     path('file/', views.serve_csv, name='serve_csv'),
     path('get-quartiers/', views.get_quartiers, name='get_quartiers'),
-    path('edit_profile/<int:user_id>', views.edit_profile, name='edit_profile'), 
+    path('edit_profile/', views.edit_profile, name='edit_profile'), 
     path('edit_profile/<int:user_id>/update', views.edit_profile_update, name='edit_profile_update'),  # AJAX endpoint for quartiers
+    path('chat/', views.chat_home, name='chat_home'),
+    path('chat/<int:user_id>/', views.chat_with_user, name='chat_with_user'),
 # AJAX endpoint for quartiers
 
 
